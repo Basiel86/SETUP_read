8ce3b251696354d493f3110a424c75ba9132c122
BaZ <Basiel@inbox.ru>
2022-05-04 16:11:58 +0300

    v 1.0

--- curren_settings_class.py
+++ curren_settings_class.py
@@ -12,7 +12,7 @@ def resource_path(relative_path):
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
-    except Exception as ex:
+    except:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
